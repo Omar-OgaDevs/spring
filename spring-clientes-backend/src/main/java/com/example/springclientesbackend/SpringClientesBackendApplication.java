c805ca86f1592160b073bc8d47295b655ba62d41
omar.ogadevs <omar.ogadevs@outlook.com>
2021-07-29 10:41:52 -0500

    backend clientes

---
+++ spring-clientes-backend/src/main/java/com/example/springclientesbackend/SpringClientesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.springclientesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClientesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringClientesBackendApplication.class, args);
+	}
+
+}
---
+++ spring-clientes-backend/src/test/java/com/example/springclientesbackend/SpringClientesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springclientesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringClientesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
